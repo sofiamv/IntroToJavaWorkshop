09c623275553e4fe738e2fa61cc1e9841cd4dca9
ColeTrammer <cole.trammer@gmail.com>
2017-04-13 14:56:39 -0700

    Day four

--- src/day3/WorldDomination.java
+++ src/day3/WorldDomination.java
@@ -5,6 +5,7 @@ import javax.swing.JOptionPane;
 public class WorldDomination {
 	public static void main(String[] args) {
 		// 1. Ask the user if they know how to write code.
+		
 
 		// 2. If they say "yes", tell them they will rule the world.
 
